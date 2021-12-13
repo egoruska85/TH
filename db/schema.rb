83a12935371d2b637af820a2732b0a98b50a69f5
egoruska85 <egorarbuzov@gmail.com>
2021-12-14 04:04:44 +0500

    Create models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_222816) do
+ActiveRecord::Schema.define(version: 2021_12_13_230255) do
+
+  create_table "purchases", force: :cascade do |t|
+    t.string "title_ru"
+    t.string "title_tm"
+    t.text "text_ru"
+    t.text "text_tm"
+    t.string "year"
+    t.string "price"
+    t.string "phone"
+    t.string "email"
+    t.boolean "bought"
+    t.boolean "cashless"
+    t.integer "type_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["type_id"], name: "index_purchases_on_type_id"
+  end
+
+  create_table "sales", force: :cascade do |t|
+    t.string "title_ru"
+    t.string "title_tm"
+    t.text "text_ru"
+    t.text "text_tm"
+    t.string "year"
+    t.string "price"
+    t.string "phone"
+    t.string "email"
+    t.boolean "sales"
+    t.boolean "cashless"
+    t.integer "type_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["type_id"], name: "index_sales_on_type_id"
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.boolean "admin"
@@ -25,4 +65,6 @@ ActiveRecord::Schema.define(version: 2021_12_13_222816) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "purchases", "types"
+  add_foreign_key "sales", "types"
 end
